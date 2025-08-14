942fe318b491953473e701bc62f808cbad93378d
BizCub <bizcubmine@gmail.com>
2025-08-14 07:24:40 +0200

    Fixed compatibility with Fabric API

--- src/main/java/com/bizcub/suggestionProvider/mixin/SuggestionProviderMixin.java
+++ src/main/java/com/bizcub/suggestionProvider/mixin/SuggestionProviderMixin.java
@@ -1,33 +1,15 @@
 package com.bizcub.suggestionProvider.mixin;
 
 import net.minecraft.command.CommandSource;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-
-import java.util.function.Consumer;
-import java.util.function.Function;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CommandSource.class)
 public interface SuggestionProviderMixin {
-    /**
-     * @author BizCub
-     * @reason Inspired by the original Suggestion Provider Fix mod by Harleyoc1
-     */
-    @Overwrite
-    static <T> void forEachMatching(Iterable<T> iterable, String string, Function<T, Identifier> function, Consumer<T> consumer) {
-        boolean inputContainsColon = string.indexOf(58) > -1;
-        for (T resource : iterable) {
-            Identifier identifier = function.apply(resource);
-            if (inputContainsColon) {
-                String identifierString = identifier.toString();
 
-                if (CommandSource.shouldSuggest(string, identifierString)) {
-                    consumer.accept(resource);
-                }
-            } else if (CommandSource.shouldSuggest(string, identifier.getNamespace()) || CommandSource.shouldSuggest(string, identifier.getPath())) {
-                consumer.accept(resource);
-            }
-        }
+    @Redirect(method = "forEachMatching(Ljava/lang/Iterable;Ljava/lang/String;Ljava/util/function/Function;Ljava/util/function/Consumer;)V", at = @At(value = "INVOKE", target = "Ljava/lang/String;equals(Ljava/lang/Object;)Z"))
+    private static boolean skipEqualCheck(String instance, Object o) {
+        return true;
     }
 }
